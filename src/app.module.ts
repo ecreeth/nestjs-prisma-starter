f605fcea8fe0054840b124a6811bb6b254b0a95b
Luis Miguel Alvarado <lalvarado@farmatrix.com.do>
2023-05-23 14:57:46 -0400

    feat: add auth `guards`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Logger, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { PrismaModule, loggingMiddleware } from 'nestjs-prisma';
 import { AuthModule } from 'src/auth/auth.module';
 import config from 'src/common/configs/config';
@@ -7,9 +9,11 @@ import { PostsModule } from 'src/posts/posts.module';
 import { UsersModule } from 'src/users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot({ ttl: 60, limit: 10 }),
     ConfigModule.forRoot({ isGlobal: true, load: [config] }),
     PrismaModule.forRoot({
       isGlobal: true,
@@ -28,6 +32,12 @@ import { AppService } from './app.service';
     PostsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
